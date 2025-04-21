a0040823a53893df34ffc9230c1362ad876a39b0
Pranay-Chauhn <pranaychauhan663@gmail.com>
2025-04-21 18:13:39 +0530

    added html file

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI, File, UploadFile
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.schemas import Review
 from app.predict import segmentation, predict_file
 
 app = FastAPI()
 
+# Allow CORS for local frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or restrict to ["http://localhost:5500"] etc.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 def home():
-    return {'message': 'API is Running ...'}
+    return {'message': 'API is Running fine ...'}
 
 
 @app.post('/predict')
